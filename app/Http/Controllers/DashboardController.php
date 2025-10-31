6c50e8a247ea9fcb10716753a422d6cbce0228cf
sudhir-rajai <rajaisudhir11@gmail.com>
2025-11-01 02:25:44 +0530

    small fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -35,6 +35,8 @@ class DashboardController extends Controller
         // Get server storage information
         $serverStorage = $this->getServerStorage();
 
+        \Spatie\ResponseCache\Facades\ResponseCache::clear();
+
         return Inertia::render('Home', [
             'stats' => $stats,
             'recentBackups' => $recentBackups,
